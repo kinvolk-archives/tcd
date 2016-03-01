f56cb4e02ec5558a4f276b4c6f1173f89f5ec520
Iago López Galeiras <iago@kinvolk.io>
2016-03-01 13:27:01 +0100

    build: build statically, shrink docker image

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 # ~/programs/protoc/protoc -I api api/service.proto --go_out=plugins=grpc:api
-go build -o ${PWD}/bin/tcd
+CGO_ENABLED=0 go build -o bin/tcd -installsuffix cgo
 sudo docker build -t kinvolk/tcd .
