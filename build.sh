4d7fa41a9f28dcfb691693dc12d2dca26cb4bb22
Alessandro Puccetti <alessandro@kinvolk.io>
2016-05-13 16:55:01 +0200

    Fixed building errors
    Update to new verison of grpc
    api/service.pb.go needed a refresh

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
+# This script is a modification of the build for docker2aci
+# https://github.com/appc/docker2aci/blob/master/build.sh
+
+set -e
+
+ORG_PATH="github.com/kinvolk"
+REPO_PATH="${ORG_PATH}/tcd"
+
+if [ ! -h gopath/src/${REPO_PATH} ]; then
+    mkdir -p gopath/src/${ORG_PATH}
+    ln -s ../../../.. gopath/src/${REPO_PATH} || exit 255
+fi
+
+export GOBIN=${PWD}/bin
+export GOPATH=${PWD}/gopath:${PWD}/Godeps/_workspace
+
+eval $(go env)
+go get -d # This will be substituted by vendoring
+
+echo "Building tcd..."
 # ~/programs/protoc/protoc -I api api/service.proto --go_out=plugins=grpc:api
-CGO_ENABLED=0 go build -o bin/tcd -installsuffix cgo
+CGO_ENABLE=0 go build -o $GOBIN/tcd -installsuffix cgo
+
 sudo docker build -t kinvolk/tcd .
